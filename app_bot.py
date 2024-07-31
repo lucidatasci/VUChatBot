a7d3835b8aa8ffac80770e58ff8c03ad0d2ae9fb
luciangus <luciana.mailme@gmail.com>
2024-07-31 00:04:14 -0300

    back to streamlit

--- app_bot.py
+++ app_bot.py
@@ -9,10 +9,10 @@ from langchain_groq import ChatGroq                               # para usar o 
 
 # Configuração da chave da API do Groq
 api_key = os.getenv("GROQ_API_KEY")
-#if api_key:
-#    os.environ["GROQ_API_KEY"] = api_key
-#else:
-#    st.error("A chave da API não está definida")
+if api_key:
+    os.environ["GROQ_API_KEY"] = api_key
+else:
+    st.error("A chave da API não está definida")
 
 # Lista com os nomes dos arquivos PDF divididos
 pdf_files = [f"pdfs/resolucao_GR-029-2024-{i}.pdf" for i in range(1, 12)]
